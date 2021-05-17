465a9c83e8845ef4668d8aee227075472100c7d6
Leo Castro <leonardo_carvalho@outlook.com>
2021-05-16 23:46:21 -0300

    removed unnecessary files

--- src/calc.ts
+++
@@ -1,5 +0,0 @@
-export default class Calc {
-  sum (value1: number, value2: number): number {
-    return value1 + value2;
-  }
-}
--- tests/calc.test.ts
+++
@@ -1,6 +0,0 @@
-import Calc from '../src/calc';
-
-test('adds 1 + 2 to equal 3', () => {
-  const calc = new Calc();
-  expect(calc.sum(1, 2)).toBe(3);
-});
