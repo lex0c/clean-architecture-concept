c86f158621bcec73aa72f00bbfd17dac85c9fd9a
Leo Castro <leonardo_carvalho@outlook.com>
2021-07-17 00:00:50 -0300

    updated field

--- src/app/database/migrations/20210711220605_create_users.ts
+++ src/app/database/migrations/20210711220605_create_users.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
 
     table.string('name').notNullable();
     table.string('email').notNullable();
-    table.string('password_hash').notNullable();
+    table.string('password').notNullable();
 
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
